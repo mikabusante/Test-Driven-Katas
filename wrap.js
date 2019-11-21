e1fe1cfeb98a19b0a65c52c5e9d029688a87c9a9
Mika Busante <mika@mikabusante.com>
2019-11-21 12:11:07 -0500

    didn't even fix anything, sorry ming :(

--- wrap.js
+++ wrap.js
@@ -1,14 +1,35 @@
 const wrap = (line, maxLength) => {
-  if(line === ''){
+  if (line === "") {
     return line;
-  }
-  else if(line.length <= maxLength){
+  } else if (line.length <= maxLength) {
     return line;
-  }
-  else if (line.length > maxLength){
-    let string = line.slice(0, maxLength) + '/n'  + line.slice(maxLength+1, -1);console.log(string);
-    return string;
-  }
+  } else if (line.length > maxLength) {
+      const finalArr = []
+      const lineArr = line.split('')
+      
+      lineArr.forEach(word => {
+        
+      })
+    
+    
+    
+    
+    //     let finalString = "";
 
-}
+    // for (let i = 0; i < line.length; i++) {
+    //   if (i < maxLength) {
+    //     finalString = finalString + line[i];
+    //   } else if (i === maxLength) {
+    //     finalString = finalString + "\n" + line[i];
+    //   } else {
+    //     finalString = finalString + line[i];
+    //   }
+    // }
+
+    // console.log(finalString);
+    // return finalString;
+    
+    
+  }
+};
 module.exports = wrap;
--- wrap.spec.js
+++ wrap.spec.js
@@ -1,12 +1,16 @@
+const { expect } = require("chai");
+const wrap = require("./wrap");
 
-const { expect } = require('chai');
-const wrap = require('./wrap');
-
-describe('wrap', () => {
-  it('Returns empty string if empty string was provided', () => {
+describe("wrap", () => {
+  it("Returns empty string if empty string was provided", () => {
     expect(wrap("", 10)).to.equal("");
   });
-  it('Returns string if string length is less than maxLength', () => {
+
+  it("Returns string if string length is less than maxLength", () => {
     expect(wrap("hello", 10)).to.equal("hello");
-  })
+  });
+
+  it("Returns the string with a line break at the max length", () => {
+    expect(wrap("hi mika", 3)).to.equal("hi \nmika");
+  });
 });
